e11412a05951f5628e04934ec53e7696cce4759a
Anuj-2770 <anujyadav221307vns@gmail.com>
2026-05-22 09:57:08 +0530

    Initial project setup - Spring Boot + MySQL connected

---
+++ src/main/java/com/smart/interview/SmartInterviewPrePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.smart.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInterviewPrePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInterviewPrePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart/interview/SmartInterviewPrePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInterviewPrePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
